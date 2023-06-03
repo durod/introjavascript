9b7f0857821bff15c2f2a2962cb304cc3163dff7
durod <durianr1989@gmail.com>
2023-06-03 13:11:00 -0600

    listo

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+const colorInput = document.getElementById('colorInput');
+        const pinturaDiv = document.getElementById('pintura');
+        
+        // Agregar un evento al input para capturar el cambio de valor
+        colorInput.addEventListener('change', function() {
+            // Obtener el color seleccionado
+            const color = colorInput.value;
+            
+            // Aplicar el color seleccionado al fondo del div
+            pinturaDiv.style.backgroundColor = color;
+        });
+
+
+
+
+
+        const btnCalcular = document.getElementById('btncalcular');
+        const resumenCompra = document.querySelector('.resumencompra');
+    
+        btnCalcular.addEventListener('click', function() {
+            const color = colorInput.value;
+            const cantidad = document.getElementById('cantidadInput').value;
+            const precio = 1000;
+            const total = precio * cantidad;
+    
+            const contenidoResumen = `
+            <h2>Tu Compra</h2>
+            <div class="compraresumendiv">
+            
+                <h4>Resumen de la compra:</h4>
+                <p>Color: <span style="background-color: ${color}; color: white;">${color}</span></p>
+                <div style="background-color: ${color}; width: 3rem; height: 3rem; border-radius:25px"; ></div>
+                <p>Cantidad: ${cantidad}</p>
+                <p>Total: ${total}$</p>
+                <button id="btncalcular">Comprar</button>
+                </div>
+            `;
+    
+            resumenCompra.innerHTML = contenidoResumen;
+        });
